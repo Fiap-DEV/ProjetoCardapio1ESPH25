861e3b32abdd9fe85b520d88f93960ad9e5eb421
Wellington <welltecnic@hotmail.com>
2025-10-20 08:28:21 -0300

    Criando a base de dados

---
+++ src/data/dados.js
@@ -0,0 +1,52 @@
+const menu=[
+    {
+        id:1,
+        titulo:"Jogo 1",
+        categoria:"acao",
+        preco:200.00,
+        img:"/src/assets/img.jpg",
+        descricao:"Jogo de ação 1"
+    },
+       {
+        id:2,
+        titulo:"Jogo 2",
+        categoria:"acao",
+        preco:20.00,
+        img:"/src/assets/img.jpg",
+        descricao:"Jogo de ação 2"
+    },
+     {
+        id:3,
+        titulo:"Jogo 3",
+        categoria:"acao",
+        preco:70.00,
+        img:"/src/assets/img.jpg",
+        descricao:"Jogo de ação 3"
+    },
+     {
+        id:4,
+        titulo:"Jogo 4",
+        categoria:"aventura",
+        preco:600.00,
+        img:"/src/assets/img.jpg",
+        descricao:"Jogo de aventura 1"
+    },
+    {
+        id:5,
+        titulo:"Jogo 5",
+        categoria:"aventura",
+        preco:600.00,
+        img:"/src/assets/img.jpg",
+        descricao:"Jogo de aventura 5"
+    },
+    {
+        id:6,
+        titulo:"Jogo 6",
+        categoria:"terror",
+        preco:250.00,
+        img:"/src/assets/img.jpg",
+        descricao:"Jogo de terror 1"
+    },
+]
+
+export default menu

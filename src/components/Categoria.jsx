b78abda470fa5fafe33227f93fec075f3d00507c
Wellington <welltecnic@hotmail.com>
2025-10-20 08:48:33 -0300

    codificando os componentes, categoria,menu,menuItem

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -1,8 +1,29 @@
 
 
-const Categoria = () => {
+const Categoria = ({categorias,filtroItems}) => {
+
+
+  const produtos =(opcao)=>{
+    switch(opcao.toLowerCase()){
+      case "acao":
+        return;
+      case "aventura":
+        return;
+      case "terror":
+        return;
+      default:
+        return null;
+    }
+  }
+
   return (
-    <div>Categoria</div>
+    <nav>
+      {categorias.map((opcao,index)=>(
+        <button key={index} onClick={()=>filtroItems(opcao)}>
+          {produtos(opcao)}
+        </button>
+      ))}
+    </nav>
   )
 }
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,14 @@
+import MenuItem from "./MenuItem"
 
-
-const Menu = () => {
+const Menu = ({items}) => {
   return (
-    <div>Menu</div>
+    <div>
+      {/* mapeia todos os items */}
+      {items.map((item)=>(
+        // chama o componente menuitem e adiciona todos os items que estiverem disponiveis
+        <MenuItem key={item} {...item}/>
+      ))}
+    </div>
   )
 }
 

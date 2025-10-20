837a9a1054bbeda4421aaba4ee46031c8bdb385d
Wellington <welltecnic@hotmail.com>
2025-10-20 09:31:25 -0300

    codificando o app e estilizando o projeto com tailwindcss

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -1,14 +1,12 @@
 
-
 const Categoria = ({categorias,filtroItems}) => {
-
 
   const produtos =(opcao)=>{
     switch(opcao.toLowerCase()){
       case "acao":
         return;
       case "aventura":
-        return;
+        return ;
       case "terror":
         return;
       default:
@@ -17,10 +15,13 @@ const Categoria = ({categorias,filtroItems}) => {
   }
 
   return (
-    <nav>
+    <nav className="flex justify-center flex-wrap gap-4 mb-12">
       {categorias.map((opcao,index)=>(
-        <button key={index} onClick={()=>filtroItems(opcao)}>
+        <button key={index} onClick={()=>filtroItems(opcao)}
+        className="bg-slate-800 text-white border-none rounded-lg px-5 py-3 cursor-pointer transition-all duration-300 hover:bg-amber-600 hover:scale-105 hover:shadow-lg"
+        >
           {produtos(opcao)}
+          {opcao}
         </button>
       ))}
     </nav>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,7 +2,7 @@ import MenuItem from "./MenuItem"
 
 const Menu = ({items}) => {
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid md:grid-cols-2 lg:grid lg:grid-cols-3 gap-8">
       {/* mapeia todos os items */}
       {items.map((item)=>(
         // chama o componente menuitem e adiciona todos os items que estiverem disponiveis
